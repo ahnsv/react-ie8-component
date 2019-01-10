7998248132851852c928d16e605d5888e35f9f09
Humphrey Ahn <ahnsv@bc.edu>
2019-01-10 13:54:02 +0900

    feature: jest configuration done

--- src/components/__test__/Sample.spec.js
+++ src/components/__test__/Sample.spec.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { shallow, mount, render } from 'enzyme';
 
 import Sample from '../Sample'
+const configure = require("enzyme").configure;
+const Adapter = require("enzyme-adapter-react-14");
+configure({ adapter: new Adapter() });
 
 describe('Sample', () => {
     it('should be defined', () => {
