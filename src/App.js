ad967436cae40bf6246bb792e786654f9979af12
Humphrey Ahn <ahnsv@bc.edu>
2019-01-19 23:42:04 +0900

    fix: edited from merge

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 const React = require('react')
 const Sample = require('./components/Sample').default
-<<<<<<< HEAD
-=======
-const Test = require('./components/Test/Test').default
->>>>>>> origin/master
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
         Hello World
-<<<<<<< HEAD
-=======
-        <Test />
->>>>>>> origin/master
         <Sample />
       </div>
     )
